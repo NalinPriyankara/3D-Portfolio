37d00369ff37b9686c82dfbf6c511c39f2f65caf
NalinPriyankara <nalinpriyankara033@gmail.com>
2024-08-23 21:22:54 +0530

    Canva

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,8 +8,36 @@ import CanvasLoader from '../Loader';
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={Math.PI} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive
+        object={computer.scene}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov:25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
